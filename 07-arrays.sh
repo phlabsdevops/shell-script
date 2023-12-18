2e2725d7b49dd3f8a2cd925367d9087f7ca522f5
phlabsdevops <harinathbeeram1999@gmail.com>
2023-12-18 15:07:31 +0530

    added 7th script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,5 +2,5 @@
 #  this is for comment
 FRUITS=("apple" "banana" "grapes")
 echo "First fruit: ${FRUITS[0]}"
-echo "First fruit: ${FRUITS[1]}"
-echo "First fruit: ${FRUITS[2]}"
+echo "Second fruit: ${FRUITS[1]}"
+echo "Third fruit: ${FRUITS[2]}"
