c272186c1ef6ad834540b1e449703da8a549e8e1
phlabsdevops <harinathbeeram1999@gmail.com>
2023-12-20 09:53:24 +0530

    added 11th script

--- 11-logs.sh
+++ 11-logs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #  this is for comment
 ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 echo "Script-name: $0"
 LOG_FILE="/tmp/$0-$TIMESTAMP.log"
@@ -8,15 +11,15 @@ LOG_FILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
 if [ $1 -ne 0 ]
 then
-echo "ERROR: $2 is fialed"
+echo " $R ERROR: $2 is fialed $N"
 exit 1
 else
-echo "$2 is success"
+echo "$G $2 is success $N"
 fi
 }
 if [ $ID -ne 0 ]
 then 
-echo "ERROR: Please run script with root access"
+echo "$R ERROR: Please run script with root access $N"
 exit 1 #give otherthan zero
 else
 echo "You are root user"
