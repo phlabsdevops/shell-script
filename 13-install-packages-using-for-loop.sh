bd8891f1969b51d62a5342bb289480f97f129d3d
phlabsdevops <harinathbeeram1999@gmail.com>
2023-12-21 15:20:46 +0530

    added 13th script

--- 13-install-packages-using-for-loop.sh
+++ 13-install-packages-using-for-loop.sh
@@ -3,11 +3,11 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-Y="\e[34m"
+Y="\e[33m"
 N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOG_FILE="/tmp/$0-$TIMESTAMP.log"
-echo "Script started executing"
+echo "Script started executing at $TIMESTAMP" &>> $LOG_FILE 
 VALLIDATE(){
     if [ $1 -ne 0 ]
     then       
@@ -24,13 +24,13 @@ exit 1
 else
 echo -e "$G You are root user $N"
 fi
-for package in $@
+for package in $@ # passed the package names as arguments
 do 
-yum list installed $package 
-if [ $? -ne 0 ]
+yum list installed $package &>> $LOG_FILE   #check installed or not 
+if [ $? -ne 0 ] #if not installed
 then 
-yum install $package -y
-VALLIDATE $? "Installation of $package is"
+yum install $package -y &>> $LOG_FILE # install the packege
+VALLIDATE $? "Installation of $package is" #validate
 else
 echo -e "$package is already installed...$Y SKIPPING $N"
 fi
