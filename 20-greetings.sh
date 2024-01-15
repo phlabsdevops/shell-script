26b9424a5725868871ba0fa5b16cdbbf1bb09eba
phlabsdevops <harinathbeeram1999@gmail.com>
2024-01-15 14:35:43 +0530

    Running shell scripts folder

--- 20-greetings.sh
+++ 20-greetings.sh
@@ -14,6 +14,16 @@ while getopts ":n:w:h" opt; do
       case $opt in 
         n) NAME="$OPTARG" ;;
         w) WISHES="$OPTARG" ;;
-        h|*) USAGE; exit;;
+        \?) echo "Imvalid Options: "$OPTARG"" >&2; USAGE; exit;;
+        :) USAGE; exit;;
+        h) USAGE; exit;;
      esac 
 done
+
+if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
+echo "ERROR: Both -n and -w are mandatory options"
+USAGE
+exit 1
+fi
+
+echo "Hello $NAME. $WISHES, I have been learning shell scripting"
