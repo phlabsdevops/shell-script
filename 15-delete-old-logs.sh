b3dd8cca3fd7eb7652b4bdf72baf54f3a5d4a7a7
phlabsdevops <harinathbeeram1999@gmail.com>
2024-01-15 10:44:05 +0530

    Running shell scripts folder

---
+++ 15-delete-old-logs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#  this is for comment
+
+SOURCE_DIR="/tmp/shellscript-logs"
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[34m"
+N="\e[0m"
+
+if [ ! -d $SOURCE_DIR ]
+then
+echo -e " $R Source Directory: $SOURCE_DIR does not exists. $N "
+fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +30 -name "*.log")
+
+while IFS= read -r line
+do
+echo "Deleted file: $line"
+rm -rf $line
+done <<< $FILES_TO_DELETE
